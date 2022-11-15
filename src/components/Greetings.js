5ccd9bff9abb56858384abc90361c3d14ffbe3bd
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-15 12:48:37 +0100

    Iteration3

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,6 +1,4 @@
 function Greetings ({lang, children}) {
-    console.log("lang: ", lang);
-    console.log("children: ", children);
     return lang === 'de'? <p>Hallo {children}</p> : <p>Bonjour {children}</p> 
 }
 
